7dab3ffc54702486e03bce68fed5429c0dc1f794
koiking213 <koiking213@gmail.com>
2020-08-27 00:01:16 +0900

    WIP: implement world

--- app/Main.hs
+++ app/Main.hs
@@ -23,7 +23,27 @@ boxWidth, boxHeight :: Float
 boxWidth  = 50
 boxHeight = 50
 
-data ObjDir = ObjUp | ObjDown | ObjRight | ObjLeft
+pickPicture :: (Picture, Picture, Picture, Picture) -> ObjDir -> Picture
+pickPicture (x, _, _, _) ObjLeft = x
+pickPicture (_, x, _, _) ObjDown = x
+pickPicture (_, _, x, _) ObjUp = x
+pickPicture (_, _, _, x) ObjRight = x
+
+type WorldHeight = Int
+type WorldWidth  = Int
+type PictureLeft  = Picture
+type PictureDown  = Picture
+type PictureUp    = Picture
+type PictureRight = Picture
+
+data World = World
+  { imageMap :: [(PictureLeft, PictureDown, PictureUp, PictureRight)]
+  , worldObjects :: [ObjState]
+  , worldSize :: (WorldHeight, WorldWidth)
+  }
+
+data ObjDir = ObjLeft | ObjDown | ObjUp | ObjRight
+data ObjKind = OHaskell deriving (Show, Enum)
 
 data ObjState = ObjState
   { objStateX  :: Float -- x 座標の位置
@@ -31,59 +51,85 @@ data ObjState = ObjState
   , objStateVx :: Float -- x 方向の速度
   , objStateVy :: Float -- y 方向の速度
   , objStateDir :: ObjDir
+  , objStateKind :: ObjKind
   }
 
+drawObj :: ObjState -> Picture -> Picture
+drawObj obj picture = translate (objStateX obj) (objStateY obj) $ scale 0.1 0.1 picture
 
-initialBox :: ObjState
-initialBox = ObjState 0 0 0 0 ObjRight
+drawWorld :: World -> IO Picture
+drawWorld world = do
+    let objPictures = [drawObj obj $ pickPicture ((imageMap world)!!(fromEnum $ objStateKind obj)) (objStateDir obj) | obj <- worldObjects world]
+    return (pictures objPictures)
 
-loadPicture :: ObjDir -> IO (Maybe Picture)
-loadPicture ObjUp    = loadJuicy "haskell_up.jpg"
-loadPicture ObjDown  = loadJuicy "haskell_down.jpg"
-loadPicture ObjRight = loadJuicy "haskell_right.jpg"
-loadPicture ObjLeft  = loadJuicy "haskell_left.jpg"
+-- | イベントを処理する関数。EventKey以外のイベントは無視する
+updateWorld :: Event -> ObjState -> IO ObjState
+updateWorld (EventKey key ks _ _) world = return $ updateWorldWithKey key ks world
+updateWorld (EventMotion _)       world = return world
+updateWorld (EventResize _)       world = return world
+
+-- | 上下左右の速度を与える関数
+up, down, right, left :: ObjState -> ObjState
+up    obj = obj { objStateVy = objStateVy obj + 100, objStateDir = ObjUp }
+down  obj = obj { objStateVy = objStateVy obj - 100, objStateDir = ObjDown }
+right obj = obj { objStateVx = objStateVx obj + 100, objStateDir = ObjRight }
+left  obj = obj { objStateVx = objStateVx obj - 100, objStateDir = ObjLeft }
+
+updateWorldWithKey :: Key -> KeyState -> World -> World
+updateWorldWithKey key ks world = world { worldObjects = (map (updateObjWithKey key ks) you) ++ remain }
+  where (you, remain) = partition (\x -> objStateKind x == OHaskell) (worldObjects world)
+
+-- | 方向キーとWASDキーに対応して四角形を移動させる
+updateObjWithKey :: Key -> KeyState -> ObjState -> ObjState
+updateObjWithKey (SpecialKey KeyUp)    ks = if ks == Down then up    else down
+updateObjWithKey (SpecialKey KeyDown)  ks = if ks == Down then down  else up
+updateObjWithKey (SpecialKey KeyRight) ks = if ks == Down then right else left
+updateObjWithKey (SpecialKey KeyLeft)  ks = if ks == Down then left  else right
+updateObjWithKey (Char 'w')            ks = if ks == Down then up    else down
+updateObjWithKey (Char 's')            ks = if ks == Down then down  else up
+updateObjWithKey (Char 'd')            ks = if ks == Down then right else left
+updateObjWithKey (Char 'a')            ks = if ks == Down then left  else right
+updateObjWithKey _ _ = id
+
+nextBox :: Float -> ObjState -> ObjState
+nextBox dt box =
+  let -- 速度を考慮した次のステップでの位置を計算
+      x  = objStateX box + objStateVx box * dt
+      y  = objStateY box + objStateVy box * dt
+
+   in box { objStateX = x, objStateY = y }
+
+nextWorld :: Float -> World -> IO World
+nextWorld dt world = world { worldObjects = map (nextBox dt) (worldObjects world) }
+
+initWorld :: World
+initWorld = World {
+  imageMap = [loadObjImage OHaskell],
+  worldObjects = [ObjState 0 0 0 0 ObjRight OHaskell],
+  worldSize = (100, 100)
+}
 
 drawBox :: ObjState -> IO Picture
 drawBox box = do
     Just img <- loadPicture (objStateDir box)
     return (translate (objStateX box) (objStateY box) $ scale 0.1 0.1 img)
 
--- | イベントを処理する関数。EventKey以外のイベントは無視する
-updateBox :: Event -> ObjState -> IO ObjState
-updateBox (EventKey key ks _ _) box = return $ updateBoxWithKey key ks box
-updateBox (EventMotion _)       box = return box
-updateBox (EventResize _)       box = return box
+loadObjImage :: ObjKind -> (PictureLeft, PictureDown, PictureUp, PictureRight)
+loadObjImage kind = do
+    Just left <- loadPicture kind ObjLeft
+    Just down <- loadPicture kind ObjDown
+    Just up <- loadPicture kind ObjUp
+    Just right <- loadPicture kind ObjRight
+    (left, down, up, right)
 
--- | 上下左右の速度を与える関数
-up, down, right, left :: ObjState -> ObjState
-up    box = box { objStateVy = objStateVy box + 100, objStateDir = ObjUp }
-down  box = box { objStateVy = objStateVy box - 100, objStateDir = ObjDown }
-right box = box { objStateVx = objStateVx box + 100, objStateDir = ObjRight }
-left  box = box { objStateVx = objStateVx box - 100, objStateDir = ObjLeft }
-
--- | 方向キーとWASDキーに対応して四角形を移動させる
-updateBoxWithKey :: Key -> KeyState -> ObjState -> ObjState
-updateBoxWithKey (SpecialKey KeyUp)    ks = if ks == Down then up    else down
-updateBoxWithKey (SpecialKey KeyDown)  ks = if ks == Down then down  else up
-updateBoxWithKey (SpecialKey KeyRight) ks = if ks == Down then right else left
-updateBoxWithKey (SpecialKey KeyLeft)  ks = if ks == Down then left  else right
-updateBoxWithKey (Char 'w')            ks = if ks == Down then up    else down
-updateBoxWithKey (Char 's')            ks = if ks == Down then down  else up
-updateBoxWithKey (Char 'd')            ks = if ks == Down then right else left
-updateBoxWithKey (Char 'a')            ks = if ks == Down then left  else right
-updateBoxWithKey _ _ = id
-
-nextBox :: Float -> ObjState -> IO ObjState
-nextBox dt box =
-  let -- 速度を考慮した次のステップでの位置を計算
-      x  = objStateX box + objStateVx box * dt
-      y  = objStateY box + objStateVy box * dt
-
-   in return box { objStateX = x, objStateY = y }
+loadPicture :: ObjKind -> ObjDir -> IO (Maybe Picture)
+loadPicture kind dir  = loadJuicy ((show kind) ++ "_" ++ (drop 3 $ show dir) ++ ".jpg")
 
 -------------
 -- main 関数
 -------------
 
 main :: IO ()
-main = playIO window black 24 initialBox drawBox updateBox nextBox
+main = let world = initWorld
+       in  playIO window black 24 world drawBox updateBox nextBox
+
