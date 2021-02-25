f0c005d399ece2558fb8f94363695b8ed49cdf4c
chilopod43 <inkstone.mukade@gmail.com>
2021-02-25 14:41:45 +0000

    xml-lensの動作確認

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,7 @@
-import Action
+{-# LANGUAGE OverloadedStrings #-}
+
+import Action ()
+import Control.Lens
 import Data.Char
 import Data.List
 import Data.List.Split
@@ -7,66 +10,42 @@ import Data.Maybe
 import Debug.Trace
 import qualified Direction as D
 import Draw
-import Graphics.Gloss
-import Graphics.Gloss.Interface.IO.Game
-import Graphics.Gloss.Juicy
-import Keyboard
 import Rule
 import Stage
 import System.Environment
 import System.Exit
+import Text.XML
+import Text.XML.Lens
 import Tile
 import Util
 import World
 
-handleEvent :: Event -> World -> IO World
-handleEvent (EventKey key Down _ _) world = do
-  let w = updateWorld key world
-  print (getRules w)
-  if win w then exitSuccess else return w
-handleEvent _ world = return world
-
-updateWorld :: Key -> World -> World
-updateWorld key world = case action of
-  Move dir -> removeUncangedWorldObjects $ defeat $ melt $ sink $ metamorphose $ walk dir $ duplicateWorldObjects world
-  Step -> metamorphose $ duplicateWorldObjects world
-  Reverse -> tailWorldObjects world
-  DoNothing -> world
-  where
-    action = keyToAction key
-
-removeUncangedWorldObjects :: World -> World
-removeUncangedWorldObjects world@(World (x : y : ys))
-  | x == y = world {worldObjectsList = y : ys}
-  | otherwise = world
-
-duplicateWorldObjects :: World -> World
-duplicateWorldObjects world@(World (x : y : _))
-  | x == y = world
-  | otherwise = world {worldObjectsList = head (worldObjectsList world) : worldObjectsList world}
-duplicateWorldObjects world = world {worldObjectsList = head (worldObjectsList world) : worldObjectsList world}
-
-tailWorldObjects :: World -> World
-tailWorldObjects world@(World [x]) = world
-tailWorldObjects world = world {worldObjectsList = tail $ worldObjectsList world}
-
-window :: Display
-window = InWindow "Haskell Is You" (windowWidth, windowHeight) (0, 0)
-
-elapseWorld :: Float -> World -> IO World
-elapseWorld dt = return
-
 main :: IO ()
 main = do
-  let objs = map OCharacter (generateEnumValues :: [Character])
-      tiles = map OTile (generateEnumValues :: [Tile])
-  objImages <- mapM loadIconImage (objs ++ tiles)
+  -- let extract_csv_from_tmx = init . init . init . drop 5
+  -- stage <- extract_csv_from_tmx . lines <$> readFile "stages/0.tmx"
+  -- print (map init stage)
 
-  args <- getArgs
-  let stagePath = case args of
-        [] -> "stages/0.csv"
-        x : xs -> x
-  stage <- words <$> readFile stagePath
-  let worldSize = (\(x : y : _) -> (x, y)) $ map read $ splitOn "," $ head stage
-  let world = initWorld worldSize $ tail stage
-  playIO window black 24 world (drawWorld worldSize $ M.fromList objImages) handleEvent elapseWorld
+  doc <- Text.XML.readFile def "stages/0.tmx"
+  let height = doc ^? root . named "map" ... named "layer" . attr "height"
+  let width = doc ^? root . named "map" ... named "layer" . attr "width"
+  let map = doc ^.. root . named "map" ... named "layer" ... named "data" . text
+  print height
+  print width
+  print map
+
+-- print width
+-- print mydata
+
+--   let objs = map OCharacter (generateEnumValues :: [Character])
+--       tiles = map OTile (generateEnumValues :: [Tile])
+--   objImages <- mapM loadIconImage (objs ++ tiles)
+
+--   args <- getArgs
+--   let stagePath = case args of
+--         [] -> "stages/0.csv"
+--         x : xs -> x
+--   stage <- words <$> readFile stagePath
+--   let worldSize = (\(x : y : _) -> (x, y)) $ map read $ splitOn "," $ head stage
+--   let world = initWorld worldSize $ tail stage
+--   playIO window black 24 world (drawWorld worldSize $ M.fromList objImages) handleEvent elapseWorld
